e7a7a6ab840e9fd9a234b358ffc6f18440d18a9a
Viktor Myronovych <viktor.myronovych@gmail.com>
2025-02-22 10:43:55 +0200

    upd msg to v6

--- src/handlers/hello/hello.go
+++ src/handlers/hello/hello.go
@@ -10,7 +10,7 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	// Set content type to plain text
 	w.Header().Set("Content-Type", "text/plain")
 	// Respond with "Hello, World!"
-	fmt.Fprintf(w, "Hello, World! Upd v5")
+	fmt.Fprintf(w, "Hello, World! Upd v6")
 }
 
 // SetupRoutes registers the "/hello" route for this feature
